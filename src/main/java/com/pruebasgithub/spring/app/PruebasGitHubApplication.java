fc6d9dcddedc3662f69d5e0219cce4a6ff830d71
MichaelPruebas <grupo6analisis@gmail.com>
2020-03-24 12:13:44 -0600

    Creando el Proyecto

---
+++ src/main/java/com/pruebasgithub/spring/app/PruebasGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.pruebasgithub.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebasgithub/spring/app/PruebasGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebasgithub.spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
